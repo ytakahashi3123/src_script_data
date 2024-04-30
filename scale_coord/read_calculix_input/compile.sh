75f961c8551b6077acb9c4b2bb841c023b5a8685
ytakahashi3123 <ytakahashi@eng.hokudai.ac.jp>
2024-04-30 17:01:36 +0900

    Change compiler

--- scale_coord/read_calculix_input/compile.sh
+++ scale_coord/read_calculix_input/compile.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-   module load oneapi/2021.3
+   #module load oneapi/2021.3
+   #module load BaseCPU
+   #source /opt/intel/oneapi/setvars.sh
 
    SOURCE=read_calculix_input.f90
    BIN_NAME=read_calculix_input
-   COMP=ifort
+#   COMP=ifort
+   COMP=gfortran
    OPT=
-
-   #source /opt/intel/compilers_and_libraries/linux/bin/compilervars.sh intel64
    
    $COMP $OPT -o $BIN_NAME $SOURCE
 
